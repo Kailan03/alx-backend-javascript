9dc305014cbfd6e6c34949938444b16fe1c9d3a7
Rahmat <rahmatbola3@gmail.com>
2024-07-04 04:01:30 +0000

    dataManipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author: Rahmat B. Kilani <https://github.com/Kailan03>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
